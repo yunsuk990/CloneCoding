a053a9631377015adcaceb7a01dc30f20f4e1476
yunsuk990 <yunsuk990@naver.com>
2022-06-28 23:50:51 +0900

    Update MainActivity.kt

--- Instagram/app/src/main/java/com/example/instagram/MainActivity.kt
+++ Instagram/app/src/main/java/com/example/instagram/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.instagram.databinding.ActivityMainBinding
+import com.example.instagram.navigation.AlarmFragment
+import com.example.instagram.navigation.DetailViewFragment
+import com.example.instagram.navigation.GridFragment
+import com.example.instagram.navigation.UserFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNav.run {
+            setOnItemSelectedListener { item ->
+                when(item.itemId){
+                    R.id.action_home -> {
+                        var detailViewFragment = DetailViewFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                    }
+                    R.id.ic_search -> {
+                        var GridFragment = GridFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_content, GridFragment).commit()
+                    }
+                    R.id.action_add_photo -> {
+
+                    }
+                    R.id.action_favorite_alarm -> {
+                        var alarmFragment = AlarmFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_content, alarmFragment).commit()
+                    }
+
+                    R.id.action_account -> {
+                        var userFragment = UserFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_content, userFragment).commit()
+                    }
+                }
+                true
+            }
+        }
     }
 }
