9689c9946fc5a962b5bb97e75b322df76bd51adb
yunsuk990 <yunsuk990@naver.com>
2022-07-02 00:20:06 +0900

    Create AddPhotoActivity.kt

---
+++ Instagram/app/src/main/java/com/example/instagram/navigation/AddPhotoActivity.kt
@@ -0,0 +1,15 @@
+package com.example.instagram.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram.R
+
+class AddPhotoActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+    }
+}
