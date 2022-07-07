094cba9bf405b7ef25eb137948507baa72b3466a
yunsuk990 <yunsuk990@naver.com>
2022-07-07 09:56:22 +0900

    Update bottomNav

--- Instagram/app/src/main/java/com/example/instagram/MainActivity.kt
+++ Instagram/app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,59 +1,66 @@
 package com.example.instagram
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.instagram.databinding.ActivityMainBinding
 import com.example.instagram.navigation.*
-import java.util.jar.Manifest
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.ic_search -> {
+                var GridFragment = GridFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, GridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                if(ContextCompat.checkSelfPermission( this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
-        binding.bottomNav.run {
-            setOnItemSelectedListener { item ->
-                when(item.itemId){
-                    R.id.action_home -> {
-                        var detailViewFragment = DetailViewFragment()
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_content, detailViewFragment).commit()
-                    }
-                    R.id.ic_search -> {
-                        var GridFragment = GridFragment()
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_content, GridFragment).commit()
-                    }
-                    R.id.action_add_photo -> {
-                        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                            startActivity(Intent(context, AddPhotoActivity::class.java))
-                        }
-                    }
-                    R.id.action_favorite_alarm -> {
-                        var alarmFragment = AlarmFragment()
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_content, alarmFragment).commit()
-                    }
-
-                    R.id.action_account -> {
-                        var userFragment = UserFragment()
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_content, userFragment).commit()
-                    }
-                }
-                true
-            }
-        }
+        binding.bottomNav.setOnItemSelectedListener(this)
+        //Set default screen
+        binding.bottomNav.selectedItemId = R.id.action_home
     }
+
 }
