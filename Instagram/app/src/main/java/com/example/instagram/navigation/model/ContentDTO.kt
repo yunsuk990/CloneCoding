03358ffd6d391cfe57973d8c05630ce2e1dfc297
yunsuk990 <yunsuk990@naver.com>
2022-07-07 09:57:47 +0900

    Update ContentDTO.kt

--- Instagram/app/src/main/java/com/example/instagram/navigation/model/ContentDTO.kt
+++ Instagram/app/src/main/java/com/example/instagram/navigation/model/ContentDTO.kt
@@ -12,8 +12,5 @@ data class ContentDTO(var explain: String? =null,
                        var userId: String? = null,
                        var comment: String? = null,
                        var timestamp: Long? = null){
-
     }
-
-
 }
