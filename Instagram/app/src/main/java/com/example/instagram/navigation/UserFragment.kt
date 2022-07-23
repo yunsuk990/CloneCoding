edd14aad7a99005cc7471799dccb7cb7d39f020f
yunsuk990 <yunsuk990@naver.com>
2022-07-23 21:45:14 +0900

    Add ProfileSettings

--- Instagram/app/src/main/java/com/example/instagram/navigation/UserFragment.kt
+++ Instagram/app/src/main/java/com/example/instagram/navigation/UserFragment.kt
@@ -1,24 +1,33 @@
 package com.example.instagram.navigation
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.circleCrop
 import com.bumptech.glide.request.RequestOptions
 import com.example.instagram.LoginActivity
 import com.example.instagram.MainActivity
 import com.example.instagram.R
 import com.example.instagram.databinding.FragmentUserBinding
 import com.example.instagram.navigation.model.ContentDTO
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 
 
 class UserFragment: Fragment() {
@@ -28,6 +37,7 @@ class UserFragment: Fragment() {
     var uid: String? = null
     var auth: FirebaseAuth? = null
     var currentUserUid: String? = null
+    private lateinit var ActivityResult: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,10 +46,12 @@ class UserFragment: Fragment() {
     ): View? {
         _binding = FragmentUserBinding.inflate(inflater, container, false)
 
+
         firestore = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
         uid = arguments?.getString("destinationUid")
         currentUserUid = auth?.currentUser?.uid
+
 
         if(uid == currentUserUid){
             //MyPage
@@ -65,7 +77,38 @@ class UserFragment: Fragment() {
         }
         binding.accountRecyclerview.adapter = UserFragmentRecyclerViewAdapter()
         binding.accountRecyclerview.layoutManager = GridLayoutManager(requireActivity(), 3)
+        binding.accountIvProfile.setOnClickListener{
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            ActivityResult = registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult()){ result ->
+                if(result.resultCode == RESULT_OK){
+                    var imageUri = result.data?.data
+                    var uid = FirebaseAuth.getInstance().currentUser?.uid
+                    var storageRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid!!)
+                    storageRef.putFile(imageUri!!).continueWithTask{ task: Task<UploadTask.TaskSnapshot> ->
+                        return@continueWithTask storageRef.downloadUrl
+                    }.addOnSuccessListener { uri ->
+                        var map = HashMap<String, Any>()
+                        map["image"] = uri.toString()
+                        firestore?.collection("profileImages")?.document(uid!!)?.set(map)
+                    }
+                }
+            }
+            ActivityResult.launch(photoPickerIntent)
+        }
+        getProfileImage()
         return binding.root
+    }
+
+    fun getProfileImage() {
+        firestore?.collection("profileImages")?.document(uid!!)?.addSnapshotListener{ documentSnapshot, firbaseFirestoreException ->
+            if(documentSnapshot == null) return@addSnapshotListener
+            if(documentSnapshot.data != null){
+                var url = documentSnapshot?.data!!["image"]
+                Glide.with(requireContext()).load(url).apply(RequestOptions().circleCrop()).into(binding.accountIvProfile)
+            }
+        }
     }
 
     inner class UserFragmentRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
